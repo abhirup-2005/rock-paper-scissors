513613d8f183883066b3636ad86b144ee5b56808
Abhirup Sengupta <217376601+abhirup-2005@users.noreply.github.com>
2025-08-02 02:13:00 +0530

    Edit script file
    added   function playRound()
            and score counter for human and computer.
    
            The function takes human & ccomputer choices and returns result

--- script.js
+++ script.js
@@ -19,3 +19,51 @@ function getHumanChoice() {
 }
 let humanChoice = getHumanChoice();
 console.log(humanChoice);
+
+
+
+let humanScore = 0,
+    computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    let firsChar = humanChoice.charAt(0).toUpperCase();
+    humanChoice = firsChar + humanChoice.slice(1);
+
+    //when choices are same
+    if(humanChoice === computerChoice) {
+        return "Its a Tie";
+    }
+
+    //when Computer Wins
+    else if(humanChoice === "Rock" && computerChoice === "Paper") {
+        computerScore++;
+        return "You lose! Paper beats Rock";
+    } else if(humanChoice === "Scissor" && computerChoice === "Rock") {
+        computerScore++;
+        return "You lose! Rock beats Scissor";
+    } else if(humanChoice === "Paper" && computerChoice === "Scissor") {
+        computerScore++;
+        return "You lose! Sissor beats Paper";
+    }
+
+    //When Human Wins
+    else if(humanChoice === "Paper" && computerChoice === "Rock") {
+        humanScore++;
+        return "You Won! Paper beats Rock";
+    } else if(humanChoice === "Rock" && computerChoice === "Scissor") {
+        humanScore++;
+        return "You Won! Rock beats Scissor";
+    } else if(humanChoice === "Scissor" && computerChoice === "Paper") {
+        humanScore++;
+        return "You Won! Sissor beats Paper";
+    }
+
+
+    else {
+        return "Invalid Input";
+    }
+
+}
+console.log(playRound(humanChoice, computerChoice));
