202f3eadbc5de400bf2fbbc0c10f154ebbfc2599
Abhirup Sengupta <217376601+abhirup-2005@users.noreply.github.com>
2025-08-02 00:23:42 +0530

    Added Blanck page and Script file
    > added   function getComputerChoice() {...}
            and a console.log(...) for debugging purpose.
    
    > linked  script file(script.js) to blanck page(index.html).

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let choice = Math.random();
+    if(choice <= 1/3) {
+        return "Rock";
+    } else if(choice > 1/3 && choice <= 2/3) {
+        return "Paper";
+    } else {
+        return "Scissor";
+    }
+}
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
